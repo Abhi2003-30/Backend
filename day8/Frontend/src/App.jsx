4a1e3fd8bd0ceba60875123860680e1c92381b1a
Abhi2003-30 <ac9805412188@gmail.com>
2026-02-03 14:06:02 +0530

    day-8

---
+++ day8/Frontend/src/App.jsx
@@ -0,0 +1,115 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+
+function App() {
+
+  const [notes, setNotes] = useState([]);
+  const [editId, setEditId] = useState(null);
+  const [editDesc, setEditDesc] = useState("");
+
+  console.log("hello integration")
+  
+  function fetchNotes() {
+    axios.get("http://localhost:3000/api/notes").then((res) => {
+      setNotes(res.data.notes);
+    });
+  }
+
+  useEffect(()=>{
+    fetchNotes();
+  }, [])
+
+  function handleSubmit(e){
+    e.preventDefault()
+
+    const {title, description} = e.target.elements
+
+    console.log(title.value, description.value)
+
+    axios.post('http://localhost:3000/api/notes',{
+      title: title.value,
+      description: description.value
+    })
+    .then(res => {
+      console.log(res.data)
+      fetchNotes()
+    })
+
+  }
+
+  function handleDeleteNote(noteId){
+    axios.delete("http://localhost:3000/api/notes/"+noteId)
+    .then(res=>{
+      console.log(res.data)
+      fetchNotes()
+    })
+  }
+
+  function handleUpdate(id) {
+    axios
+      .patch(`http://localhost:3000/api/notes/${id}`, {
+        description: editDesc,
+      })
+      .then(() => {
+        fetchNotes(); // refresh notes
+        setEditId(null); // exit edit mode
+        setEditDesc("");
+      })
+      .catch((err) => console.log(err));
+  }
+
+
+
+  return (
+    <>
+
+    <form className="note-create-form" onSubmit={handleSubmit}>
+      <input name="title" type="text" placeholder="Enter title" />
+      <input name="description" type="text" placeholder="Enter description" />
+      <button>Create Note</button>
+    </form>
+
+    <div className="notes">
+      {notes.map(note => {
+       return (
+         <div className="note">
+           <h1>{note.title}</h1>
+           {editId === note._id ? (
+             <>
+               <input
+                 type="text"
+                 value={editDesc}
+                 onChange={(e) => setEditDesc(e.target.value)}
+               />
+               <button onClick={() => handleUpdate(note._id)}>Save</button>
+               <button onClick={() => setEditId(null)}>Cancel</button>
+             </>
+           ) : (
+             <p>{note.description}</p>
+           )}
+           <div className="btn">
+             <button
+               onClick={() => {
+                 handleDeleteNote(note._id);
+               }}
+             >
+               Delete
+             </button>
+             <button
+               onClick={() => {
+                 setEditId(note._id);
+                 setEditDesc(note.description);
+               }}
+             >
+               Edit Description
+             </button>
+           </div>
+         </div>
+       );
+      })}
+    </div>
+    </>
+  )
+}
+
+export default App
