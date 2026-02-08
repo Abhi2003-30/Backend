d11e251e0a817d91b73603e21599793e87fc8e66
Abhi2003-30 <ac9805412188@gmail.com>
2026-02-08 21:31:27 +0530

    user register and login

---
+++ day13/src/routes/auth.routes.js
@@ -0,0 +1,74 @@
+const express = require('express')
+const userModel = require('../models/user.model')
+const jwt = require('jsonwebtoken')
+const authRouter = express.Router()
+const crypto = require('crypto')
+
+authRouter.post("/register", async (req, res)=>{
+    const {name, email, password} = req.body
+
+    const isUserAreadyExist = await userModel.findOne({email})
+
+    if(isUserAreadyExist){
+        return res.status(409).json({
+            message: "user already exists with this email address"
+        })
+    }
+
+    const hash = crypto.createHash('md5').update(password).digest('hex')
+
+    const user = await userModel.create({
+        name, email, password: hash
+    })
+
+    const token = jwt.sign(
+        {
+            id: user._id,
+            email: user.email
+        },
+        process.env.JWT_SECRET
+    )
+
+    res.cookie("jwt_token", token)
+
+    res.status(201).json({
+        message: "user register",
+        user,
+        token
+    })
+})
+
+authRouter.post("/login", async (req, res) =>{
+    const {email, password} = req.body
+
+    const user = await userModel.findOne({email})
+
+    if(!user){
+        return res.status(404).json({
+            message: "user not found with this email address"
+        })
+    }
+
+    const isPasswordMatched = user.password === crypto.createHash('md5').update(password).digest('hex')
+
+    if(!isPasswordMatched) {
+        return res.status(401).json({
+            message: "Invalid Password"
+        })
+    }
+
+    const token = jwt.sign({
+        id: user._id,
+    },
+    process.env.JWT_SECRET
+    )
+
+    res.cookie("jwt_token", token)
+
+    res.status(200).json({
+        message: "user loggedIn",
+        token
+    })
+})
+
+module.exports = authRouter
