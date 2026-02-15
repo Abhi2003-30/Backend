adae790172a7c3df9fad10a3750a215896960881
Abhi2003-30 <ac9805412188@gmail.com>
2026-02-15 11:06:15 +0530

    Day-17 get posts and get post details

--- day14/src/controllers/post.controller.js
+++ day14/src/controllers/post.controller.js
@@ -50,6 +50,81 @@ async function createPostController(req,res){
 
 }
 
+async function getPostController(req,res) {
+    const token = req.cookies.token
+
+    if(!token){
+        return res.status(401).json({
+            message: "unauthorized access."
+        })
+    }
+
+    let decoded;
+    try {
+         decoded = jwt.verify(token, process.env.JWT_SECRET)
+    } catch (err) {
+        return res.status(401).json({
+            mrssage: "Invalid token."
+        })
+    }
+
+    const userId = decoded.id
+
+    const posts = await postModel.find({
+        user: userId
+    })
+
+    res.status(200).json({
+        message: "posts fetched successfully",
+        posts
+    })
+}
+
+async function getPostDetailscontroller(req,res) {
+    const token = req.cookies.token
+
+    if(!token){
+        return res.status(401).json({
+            message: "unauthorized access."
+        })
+    }
+
+    let decoded;
+    try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+    } catch (err) {
+        return res.status(401).json({
+            message: "Invalid token."
+        })
+    }
+
+    const userId = decoded.id
+    const postId = req.params.postId
+
+    const post = await postModel.findById(postId)
+
+    if(!post){
+        return res.status(404).json({
+            message: "Post not found"
+        })
+    }
+
+    const isValidUser = post.user.toString() === userId
+
+    if(!isValidUser){
+        return res.status(403).json({
+            message: "forbidden content."
+        })
+    }
+
+    return res.status(200).json({
+        message: "post fetched sucessfully",
+        post
+    })
+}
+
 module.exports = {
-    createPostController
+    createPostController,
+    getPostController,
+    getPostDetailscontroller
 }
--- day14/src/routes/post.route.js
+++ day14/src/routes/post.route.js
@@ -1,14 +1,24 @@
-const express = require("express")
-const postRouter = express.Router()
-const postController = require("../controllers/post.controller")
-const multer = require("multer")
-const upload = multer({storage: multer.memoryStorage()})
-
+const express = require("express");
+const postRouter = express.Router();
+const postController = require("../controllers/post.controller");
+const multer = require("multer");
+const upload = multer({ storage: multer.memoryStorage() });
 
 // POST /api/posts [protected]
 // req.body = {caption image-file}
 // /api/posts/
 
-postRouter.post("/",upload.single("image"), postController.createPostController)
+postRouter.post(
+  "/",
+  upload.single("image"),
+  postController.createPostController,
+);
 
-module.exports = postRouter
+// GET /api/posts/ [protected]
+postRouter.get("/", postController.getPostController);
+
+// GET /api/posts/details/:postId
+// return an detail about specific post with the id. also check whether the post belongs to the user that the request comes from
+postRouter.get("/details/:postId", postController.getPostDetailscontroller)
+
+module.exports = postRouter;
